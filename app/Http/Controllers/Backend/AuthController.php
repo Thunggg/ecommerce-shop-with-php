ceb71313d7754191e32bb26af04e3576082e5f7e
Thunggg <thuannguyen20041028@gmail.com>
2025-04-12 13:12:07 +0700

    init project

---
+++ app/Http/Controllers/Backend/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(){
+        echo 1;
+        die();
+    }
+}
